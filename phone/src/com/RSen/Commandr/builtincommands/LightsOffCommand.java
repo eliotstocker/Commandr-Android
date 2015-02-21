2c8a599be3f8290d0ae990630c156c2b9829f129
Eliot Stocker <eliot@piratemedia.tv>
2015-02-21 19:53:35 +0000

    Add Turn on and off commands for users that have Light Controller 1.7+ installed using new API

---
+++ phone/src/com/RSen/Commandr/builtincommands/LightsOffCommand.java
@@ -0,0 +1,92 @@
+package com.RSen.Commandr.builtincommands;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.RSen.Commandr.R;
+import com.RSen.Commandr.core.MostWantedCommand;
+
+import tv.piratemedia.lightcontroler.api.LightControllerAPI;
+import tv.piratemedia.lightcontroler.api.LightControllerException;
+import tv.piratemedia.lightcontroler.api.LightZone;
+
+/**
+ * @author Eliot Stocker
+ * @version 1.0 Feb 21st 15
+ */
+public class LightsOffCommand extends MostWantedCommand {
+
+    private static String TITLE;
+    private static String DEFAULT_PHRASE;
+    private Context context;
+
+    public LightsOffCommand(Context ctx) {
+        DEFAULT_PHRASE = ctx.getString(R.string.lights_off_pharse);
+        TITLE = ctx.getString(R.string.lights_off_title);
+        context = ctx;
+    }
+
+    @Override
+    public void execute(final Context context, String predicate) {
+
+        LightControllerAPI api;
+        try {
+             api = new LightControllerAPI(context);
+        } catch (LightControllerException e) {
+            if(e.getCode() == LightControllerException.TYPE_APPLICATION_OLD) {
+                Toast.makeText(context, context.getString(R.string.lights_controller_old), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, context.getString(R.string.lights_controller_not_installed), Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        
+        LightZone[] zones = api.listZones();
+        Boolean found = false;
+        LightZone zone = null;
+        for(int i = 0; i < zones.length; i++) {
+            if(zones[i].Name.toLowerCase().equals(predicate)) {
+                found = true;
+                zone = zones[i];
+            }
+        }
+        
+        if(!found) {
+            Toast.makeText(context, String.format(context.getString(R.string.light_zone_not_found), predicate), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        
+        api.lightsOff(zone);
+    }
+
+    @Override
+    public boolean isAvailable(Context context) {
+        try {
+            new LightControllerAPI(context);
+        } catch(LightControllerException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String getTitle() {
+        return TITLE;
+    }
+
+    @Override
+    public String getPredicateHint() {
+        return context.getString(R.string.light_zone_hint);
+    }
+
+    @Override
+    public String getDefaultPhrase() {
+        return DEFAULT_PHRASE;
+    }
+
+
+    @Override
+    public boolean isHandlingGoogleNowReset() {
+        return true;
+    }
+}
--- phone/src/com/RSen/Commandr/core/MostWantedCommands.java
+++ phone/src/com/RSen/Commandr/core/MostWantedCommands.java
@@ -19,6 +19,8 @@ import com.RSen.Commandr.builtincommands.FlashlightOffCommand;
 import com.RSen.Commandr.builtincommands.GPSCommand;
 import com.RSen.Commandr.builtincommands.GPSOffCommand;
 import com.RSen.Commandr.builtincommands.GoodNightCommand;
+import com.RSen.Commandr.builtincommands.LightsOffCommand;
+import com.RSen.Commandr.builtincommands.LightsOnCommand;
 import com.RSen.Commandr.builtincommands.LockCommand;
 import com.RSen.Commandr.builtincommands.LowerVolumeCommand;
 import com.RSen.Commandr.builtincommands.NextMusicCommand;
@@ -86,7 +88,7 @@ public class MostWantedCommands {
                     new RestartNowRootOnly(context), new ClearNotificationsCommand(context), new WolframRedirectCommand(context), new SendWhatsappCommand(context), new RotationLockOnCommand(context),
                     new RotationLockOffCommand(context), new SyncCommand(context), new SyncOffCommand(context), new GoodNightCommand(context), new AirplaneCommand(context),
                     new AirplaneOffCommand(context), new CarModeCommand(context), new CarModeOffCommand(context), new ThankYouGoogleCommand(context),new ScreenBrightnessCommand(context), new BatteryCommand(context)
-            , new AudioCaptureCommand(context), new WazeNavigateCommand(context)};
+            , new AudioCaptureCommand(context), new WazeNavigateCommand(context), new LightsOnCommand(context), new LightsOffCommand(context)};
         }
         return commands;
     }
